9be7b5e5a4b238475e40a082059d41dfb356714c
Philipp Drüke <philipp.drueke@informatik.hs-fulda.de>
2024-02-22 21:05:49 +0100

    added wifi library

--- src/main.c
+++ src/main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <wifi.h>
 
 void app_main() {
     
+    esp_err_t ret = nvs_flash_init(); // Initialize NVS Flash memory for the WIFI credentials
+
+    // Check for NVS initialization errors
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND)
+    {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret); // Check for general initialization errors
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    init_wifi();
+
+    if(wifi_established){
+
+    }
 }
