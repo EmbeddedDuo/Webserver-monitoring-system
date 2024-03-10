108e039ec3038b1922931cae58f5f26f24c61706
Philipp Drüke <philipp.drueke@informatik.hs-fulda.de>
2024-03-10 23:18:07 +0100

    docu for whatsapp component

--- components/whatsapp_messaging_http/whatsapp_messaging_http.c
+++ components/whatsapp_messaging_http/whatsapp_messaging_http.c
@@ -1,23 +1,34 @@
 #include "whatsapp_messaging_http.h"
 
 static const char *TAG = "HTTP_CLIENT";
+
+//callmebot api specific values
 const char *api_key = CONFIG_API_KEY;
 const char *whatsapp_num = CONFIG_WHATSAPP_NUM;
 const char *whatsapp_message = CONFIG_WHATSAPP_MESSAGE;
 
 void send_whatsapp_message()
 {
+
+    // callmebot api uri without values
     char callmebot_url[] = "https://api.callmebot.com/whatsapp.php?phone=%s&text=%s&apikey=%s";
 
+    // uri with values
     char URL[strlen(callmebot_url)];
+
+    // use sprintf function to set values to URL variable
     sprintf(URL, callmebot_url, whatsapp_num, url_encode(whatsapp_message), api_key);
     ESP_LOGI(TAG, "URL = %s", URL);
+
+    //set URL with values and http request method
     esp_http_client_config_t config = {
         .url = URL,
         .method = HTTP_METHOD_GET,
     };
 
     esp_http_client_handle_t client = esp_http_client_init(&config);
+
+    //perform http request
     esp_err_t err = esp_http_client_perform(client);
 
     if (err == ESP_OK)
